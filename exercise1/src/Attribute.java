24adacf3aa78494028838a669e5727171a65487d
MessineseGianluca <gianluca.messi1@gmail.com>
2018-04-20 13:09:02 +0200

    Implement class Attribute

--- exercise1/src/Attribute.java
+++ exercise1/src/Attribute.java
@@ -1,3 +1,22 @@
-public class Attribute {
-
+public abstract class Attribute {
+  
+	protected String name; // symbolic name of the attribute
+	protected int index; // numeric id of the attribute
+	
+	Attribute(String name, int index) {
+		this.name = name;
+		this.index = index;
+	}
+	
+    String getName() {
+		return name;
+	}
+	
+	int getIndex() {
+		return index;
+	}
+	
+	public String toSTring() {
+		return name;
+	}
 }
---
+++ exercise1/src/ContinuousAttribute.java
@@ -0,0 +1,3 @@
+public class ContinuousAttribute extends Attribute {
+    
+}
