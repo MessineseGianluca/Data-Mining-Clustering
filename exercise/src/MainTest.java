953b8c585947e42441b17ed88fc5a4bc05fb067f
Giuseppe Superbo <giuseppe.superbo97@gmail.com>
2018-05-04 13:51:55 +0200

    Added user interaction feature so he can give in input the number of clusters

--- exercise/src/MainTest.java
+++ exercise/src/MainTest.java
@@ -1,14 +1,25 @@
 import data.Data;
+import keyboardinput.Keyboard;
 import mining.KmeansMiner;
 
 public class MainTest {
 	public static void main(String[] args) {
 		Data data = new Data();
-		int k = 3; // 3 clusters
-		System.out.println(data);
-		KmeansMiner kmeans = new KmeansMiner(k);
-		int numIter = kmeans.kmeans(data);
-		System.out.println("Numero di Iterazioni: " + numIter);
-		System.out.println(kmeans.getC().toString(data));	
+		boolean flag=false;
+		while(!flag) {
+			System.out.println("Quanti cluster vuoi costruire? \n");
+			int k = Keyboard.readInt(); // Number of clusters to be built
+			System.out.println(data);
+			KmeansMiner kmeans = new KmeansMiner(k);
+			int numIter = kmeans.kmeans(data);
+			System.out.println("Numero di Iterazioni: " + numIter);
+			System.out.println(kmeans.getC().toString(data));
+			System.out.println("Vuoi ripetere l'iterazione con un numero diverso di cluster? (Y/N) \n");
+			char choice = Keyboard.readChar();
+			if(choice=='N') {
+				flag=true;
+			}
+		}
+		
 	}
 }
