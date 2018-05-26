dcdebfc73c6871bdf77e2a5974f5c0972815d399
MessineseGianluca <gianluca.messi1@gmail.com>
2018-05-26 10:25:51 +0200

    Fix indentation of Main

--- exercise/src/MainTest.java
+++ exercise/src/MainTest.java
@@ -5,41 +5,30 @@ import keyboardinput.Keyboard;
 import mining.KmeansMiner;
 import data.Data;
 import data.OutOfRangeSampleSize;
-
-
 public class MainTest {
-
-	/**
-	 * @param args
-	 */
-	
-	private static int menu(){
+	private static int menu() {
 		int answer;
 		System.out.println("Select an operation");
-		do{
+		do {
 			System.out.println("(1) Load cluster from file");
 			System.out.println("(2) Load data");
 			System.out.print("Answer:");
-			answer=Keyboard.readInt();
-		}
-		while(answer<=0 || answer>2);
+			answer = Keyboard.readInt();
+		} while(answer <= 0 || answer > 2);
 		return answer;
-		
 	}
 	
-	static KmeansMiner learningFromFile() throws FileNotFoundException, IOException, ClassNotFoundException{
-		String fileName="";
-		System.out.print("Archive name:");
-		fileName=Keyboard.readString();
-		return new KmeansMiner(fileName+".dmp");
+	static KmeansMiner learningFromFile() throws FileNotFoundException, IOException, ClassNotFoundException {
+		String fileName = "";
+		System.out.print("Archive name: ");
+		fileName = Keyboard.readString();
+		return new KmeansMiner(fileName + ".dmp");
 		
 	}
 	public static void main(String[] args) {
-		
-		do{
-			int menuAnswer=menu();
-			switch(menuAnswer)
-			{
+		do {
+			int menuAnswer = menu();
+			switch(menuAnswer) {
 				case 1:
 					try {
 						KmeansMiner kmeans=learningFromFile();
@@ -53,22 +42,21 @@ public class MainTest {
 					}
 					break;
 				case 2:
-					Data data =new Data();
+					Data data = new Data();
 					System.out.println(data);
-					char answer='y';
-					do{
-						int k=1;
-						System.out.print("Insert k:");
-						k=Keyboard.readInt();
-						KmeansMiner kmeans=new KmeansMiner(k);
-						try
-						{
-							int numIter=kmeans.kmeans(data);
-							System.out.println("Number of iteration:"+numIter);
+					char answer = 'y';
+					do {
+						int k = 1;
+						System.out.print("Insert k: ");
+						k = Keyboard.readInt();
+						KmeansMiner kmeans = new KmeansMiner(k);
+						try {
+							int numIter = kmeans.kmeans(data);
+							System.out.println("Number of iteration:" + numIter);
 							System.out.println(kmeans.getC().toString(data));
 							System.out.print("Backup file name:");
-							String fileName=Keyboard.readString()+".dmp";
-							System.out.println("Saving in "+fileName);
+							String fileName = Keyboard.readString() + ".dmp";
+							System.out.println("Saving in " + fileName);
 							try {
 								kmeans.save(fileName);
 							} catch (FileNotFoundException e) {
@@ -79,27 +67,18 @@ public class MainTest {
 								e.printStackTrace();
 							}
 							System.out.println("End of saving operations!");
-							
-							
-						}
-						catch(OutOfRangeSampleSize e)
-						{
+						} catch(OutOfRangeSampleSize e) {
 							System.out.println(e.getMessage());
 						}
-						System.out.print("Do you want to continue?(y/n)");
-						answer=Keyboard.readChar();
-					}
-					while(answer=='y');
+						System.out.print("Do you want to continue? (y/n): ");
+						answer = Keyboard.readChar();
+					} while(answer == 'y');
 					break;
 				default:
 					System.out.println("Invalid operation!");
-		
 			}
-			
-			System.out.print("Do you want to choose another operation from the menÃ¹?(y/n)");
-			if(Keyboard.readChar()!='y')
-				break;
-			}
-		while(true);
-		}
+			System.out.print("Do you want to choose another operation from the menù?(y/n): ");
+			if(Keyboard.readChar() != 'y') break;
+		} while(true);
 	}
+}
