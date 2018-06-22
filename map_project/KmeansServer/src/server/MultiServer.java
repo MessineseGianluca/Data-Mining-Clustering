364a946db33143bb094d4b2107abbbc93a69f2bb
RanaAlessandro <alexhey14@gmail.com>
2018-06-22 19:10:05 +0200

    Init package server

--- map_project/KmeansServer/src/server/MultiServer.java
+++ map_project/KmeansServer/src/server/MultiServer.java
@@ -1,5 +1,35 @@
 package server;
 
-public class MultiServer {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class MultiServer {
+    private int PORT = 8080;
+    
+    public MultiServer(int port) {
+        this.PORT = port;
+        this.run();
+    }
+  
+    public void run() {
+        ServerSocket s = new ServerSocket(PORT);
+        System.out.println("Server Started");
+        try {
+            while(true) {
+                Socket socket = s.accept();
+                try {
+                    new ServerOneClient(socket);
+                } catch(IOException e) {
+                    socket.close();
+                }
+            }
+        } finally {
+            s.close();
+        }
+    }
+
+    public static void main(String[] args){
+        int port = 8080;
+        new MultiServer(port);
+    } 
 }
--- map_project/KmeansServer/src/server/ServerOneClient.java
+++ map_project/KmeansServer/src/server/ServerOneClient.java
@@ -1,5 +1,37 @@
 package server;
 
-public class ServerOneClient {
+import mining.KmeansMiner;
+import java.io.*;
+import java.net.Socket;
+
+public class ServerOneClient extends Thread {
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private KmeansMiner kmeans;
+    
+    public ServerOneClient(Socket s) throws IOException {
+        socket = s;
+        in = new ObjectInputStream(socket.getInputStream());
+        out = new ObjectOutputStream(socket.getOutputStream());
+        start(); 
+    }
+        
+    public void run() {
+        try {
+            while(true) {
+                /* ... */ 
+            }
+            System.out.println("Closing request...");
+        } catch(IOException e) {
+            System.err.println("IO Exception");
+        } finally {
+            try {
+                socket.close();
+            } catch(IOException e) {
+                System.err.println("Socket not closed");
+            }  
+        }
+    }
 
 }
