49ace0a3d061e7a57c1c69fa30b058af1fe70f52
Giuseppe Superbo <giuseppe.superbo97@gmail.com>
2018-07-01 12:32:32 +0200

    Add new documentation to kMeans Client

--- map_project/KmeansClient/src/client/KMeans.java
+++ map_project/KmeansClient/src/client/KMeans.java
@@ -17,7 +17,7 @@ import java.io.*;
  * can be requested by the user. This class extends the JApplet class in order to
  * use all the methods needed for the creation of the interface. In addition this
  * class encapsulates also methods to establish connection with the server in order
- * to accomplish all the requests.
+ * to accomplish all the requests. 
  */
 public class KMeans extends JApplet {
 	/**
@@ -29,11 +29,15 @@ public class KMeans extends JApplet {
     /**
      * The Object Output Stream out is used to print all the output received 
      * from the server after a request.
+     * 
+     * @see ObjectOutputStream
      */
     private ObjectOutputStream out;
     
     /**
-     * The Object Input Stream in is used to send a request to the server
+     * The Object Input Stream in is used to send a request to the server.
+     * 
+     * @see ObjectInputStream
      */
     private ObjectInputStream in;
     
@@ -43,7 +47,9 @@ public class KMeans extends JApplet {
      * methods from JPanel to add inner components such as JButtons and JPanels. 
      * It instantiates all the Action Listener that makes useful the interface.
      * These Action Listeners call methods that connect the client to the server 
-     * to solve requests.	
+     * to solve requests.
+     * 
+     * @see JPanel	
      */
     class TabbedPane extends JPanel {
     	
@@ -56,14 +62,18 @@ public class KMeans extends JApplet {
         /**
          * PanelWrite is the JPanel where the user can request a computation of
          * k clusters of a defined table which is saved on the DataBase. Furthermore
-         * the user have to choose a valid name to save his computation in a file. 
+         * the user have to choose a valid name to save his computation in a file.
+         * 
+         *  @see JPanelWrite
          */
         private JPanelWrite panelWrite;
         
         /**
          * PanelRead is the JPanel where the user can request and visualize 
          * a past computation which is saved in a File. The name of this File 
-         * must be given by the user. 
+         * must be given by the user.
+         * 
+         *  @see JPanelRead
          */
         private JPanelRead panelRead;
         
@@ -78,12 +88,21 @@ public class KMeans extends JApplet {
         public TabbedPane() {
         	
         	/**
-        	 * Construction of a new JTabbedPane object from JSwing
+        	 * Construction of a new JTabbedPane object from JSwing.
+        	 * 
+        	 * @see JTabbedPane
         	 */
             JTabbedPane tabbedPane = new JTabbedPane();
             
             /**
-             * actionWrite Action Listener is linked to 
+             * actionWrite Action Listener is linked to a JButton of the WriteFile
+             * JPannel. It invokes the learningFromDBAction method which establishes a
+             * connection with the server in order to get data from the database.
+             * If an IOException or a ClassNotFoundException occurs, it will generate
+             * a MessageDialog (made from a JOptionPane) which shows the error message.
+             * 
+             * @see JOptionPane
+             * @see ActionListener
              */
             ActionListener actionWrite = new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
@@ -96,6 +115,17 @@ public class KMeans extends JApplet {
                     }
                 }
             };
+            
+            /**
+             * actionRead Action Listener is linked to a JButton of the ReadFile
+             * JPannel. It invokes the learningFromFileAction method which establishes a
+             * connection with the server in order to get data from a file.
+             * If an IOException or a ClassNotFoundException occurs, it will generate
+             * a MessageDialog (made from a JOptionPane) which shows the error message.
+             * 
+             * @see JOptionPane
+             * @see ActionListener
+             */
             ActionListener actionRead = new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     try {
@@ -107,19 +137,77 @@ public class KMeans extends JApplet {
                     }  
                 }
             };
+            
+            /**
+             * Construct a new JPanelWrite by setting a String for the JButton of the JPanel
+             * and an ActionListener linked also to this JButton.
+             * 
+             * @see JPanelWrite
+             */
             this.panelWrite = new JPanelWrite("Send", actionWrite);
+            
+            /**
+             * Construct a new JPanelRead by setting a String for the JButton of the JPanel
+             * and an ActionListener linked also to this JButton.
+             * 
+             * @see JPanelRead
+             */
             this.panelRead = new JPanelRead("Send", actionRead);
+            
+            /**
+             * The method addTab of the tabbedPane object adds panelWrite JPanel to the main
+             * tabbePane by setting also the label for the corresponding tab.
+             */
             tabbedPane.addTab("Write to File", panelWrite);
+            
+            /**
+             * The method addTab of the tabbedPane object adds panelRead JPanel to the main
+             * tabbePane by setting also the label for the corresponding tab.
+             */
             tabbedPane.addTab("Read File", panelRead);
             try {
+            	
+            	/**
+            	 * The method setTabComponentAt sets a Label to the corresponding tab of the
+            	 * first integer parameter. The second parameter contains a reference to
+            	 * a JPanel which is returned by the getLabel method.
+            	 * 
+            	 * @see getLabel
+            	 */
                 tabbedPane.setTabComponentAt(0, getLabel("WriteToFile", "/icons/db1.png"));
+                
+                /**
+            	 * The method setTabComponentAt sets a Label to the corresponding tab of the
+            	 * first integer parameter. The second parameter contains a reference to
+            	 * a JPanel which is returned by the getLabel method.
+            	 * 
+            	 * @see getLabel
+            	 */
                 tabbedPane.setTabComponentAt(1, getLabel("Read File", "/icons/ld1.png"));
+              
+              /*
+               * This catch block checks if the getLabel method has no IOException during
+               * his activity.
+               */
             } catch(IOException ex) {
             	ex.printStackTrace();
             }
+            
+            /**
+             * The tabbedPane is added to the main frame of the client interface
+             */
             this.add(tabbedPane);
         }
-    
+        
+        /**
+         * This method takes an icon from a file and creates a JLabel with
+         * the icon and the string given by the caller.
+         * 
+         * @param title, it's the string that will compose the label
+         * @param icon, it's the file path of the icon that will compose the label
+         * @return JLabel which is composed by the icon and the title string
+         * @throws IOException, if the given path of the icon doesn't exist
+         */
         public JLabel getLabel(String title, String icon) throws IOException {
         	JLabel label = new JLabel(title);
         	try {
