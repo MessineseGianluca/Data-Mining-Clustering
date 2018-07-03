f90e12c6392d867bf2e6dcb77a5a62c5b52f86c1
RanaAlessandro <alexhey14@gmail.com>
2018-07-03 16:19:14 +0200

    Add doc to server package

---
+++ map_project/KmeansServer/src/server/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the classes needed to start the execution
+ * of a new server application and to handle the communication
+ * with a specific client.
+ */
+package server;
